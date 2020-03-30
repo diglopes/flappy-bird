180c2f5005dc5cb84cd106a29431acaa90356269
Diego Lopes <diego-chez@hotmail.com>
2020-03-30 09:21:18 -0300

    add flappy bird object and render it on canvas

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ sprites.src = "./assets/sprites.png";
 
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
+
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  width: 33,
+  height: 24,
+  positionX: 10,
+  positionY: 50,
+  draw() {
+    ctx.drawImage(
+      sprites,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height,
+      this.positionX,
+      this.positionY,
+      this.width,
+      this.height
+    );
+  }
+};
+
+function renderLoop() {
+  flappyBird.draw();
+  requestAnimationFrame(renderLoop);
+}
+
+renderLoop();
