5073ceeb9912dfa914381dd402fe2c6ba25ee44b
Diego Lopes <diego-chez@hotmail.com>
2020-03-30 10:17:06 -0300

    add floor object and render it on canvas

--- script.js
+++ script.js
@@ -26,7 +26,41 @@ const flappyBird = {
   }
 };
 
+const floor = {
+  spriteX: 0,
+  spriteY: 610,
+  width: 224,
+  height: 112,
+  positionX: 0,
+  positionY: canvas.height - 112,
+  draw() {
+    ctx.drawImage(
+      sprites,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height,
+      this.positionX,
+      this.positionY,
+      this.width,
+      this.height
+    );
+
+    ctx.drawImage(
+      sprites,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height,
+      this.positionX + this.width,
+      this.positionY,
+      this.width,
+      this.height
+    );
+  }
+};
 function renderLoop() {
+  floor.draw();
   flappyBird.draw();
   requestAnimationFrame(renderLoop);
 }
